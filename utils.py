f6130fc7d8fd47ed68b7d82f7b989f1b5ee1e0c8
sonic kuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-08-22 00:30:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
 from config import ADMINS
 
-def is_admin(user_id):
-    return user_id in ADMINS
+#def is_admin(user_id):
+
+#    return user_id in ADMINS
+
+
+def is_admin(user_id: int) -> bool:
+    from config import ADMINS
+    # Convert comma-separated string to list of integers
+    return user_id in [int(admin_id.strip()) for admin_id in ADMINS.split(',')]
